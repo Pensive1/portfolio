d4907d892cf177cc9cb657fd80af41b42d8263e3
Richard Acquaye <racquaye89@gmail.com>
2023-08-13 22:42:05 +0100

    hotfix: Add Analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Outfit } from "next/font/google";
 import "../globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
