c8b8d59cb00d13dbb835fccfb52ccea8f4ab966b
Richard Acquaye <racquaye89@gmail.com>
2023-12-01 00:32:28 +0000

    feat: Add blog link + icon to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import FooterIconLink from "./FooterIconLink";
 
 export default function Footer() {
   return (
-    <footer className="p-4 flex flex-col items-center max-xl:gradient-border md:p-0 xl:w-[100vw]">
+    <footer className="footer p-4 flex flex-col items-center max-xl:gradient-border md:p-0 xl:w-[100vw]">
       <div className="flex flex-col gap-6 md:py-4 2xl:flex-row-reverse justify-between xl:gradient-border xl:w-[var(--1280-wrapper)] 2xl:w-[var(--1440-wrapper)]">
         <div className="flex gap-8 justify-center md:gap-[4.44rem]">
           <FooterIconLink
@@ -18,6 +18,12 @@ export default function Footer() {
             iconType="linkedin"
           >
             LinkedIn
+          </FooterIconLink>
+          <FooterIconLink
+            href={"https://medium.com/@racquaye89"}
+            iconType="medium"
+          >
+            Blog
           </FooterIconLink>
           <FooterIconLink
             href={"mailto:richard.acquaye@hotmail.co.uk"}
