6ab272c8563278e22b6e23efcd40f1f237834446
Richard Acquaye <racquaye89@gmail.com>
2024-05-08 13:44:35 +0100

    fix: Prevent link prefetching

--- components/Button.tsx
+++ components/Button.tsx
@@ -26,6 +26,7 @@ export default function Button({
       className={`cta-btn ${buttonStyle[btnType]} w-full`}
       target={linkType === "external" ? "_blank" : "_self"}
       download={isFile}
+      rel={linkType === "external" ? "noopener noreferrer" : ""}
     >
       {showIcon && <Icon iconType={iconType} size={size} use="button" />}
       {children}
