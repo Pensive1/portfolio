a6dbd14062237838dc8a8b0af8be483d824ffabd
Richard Acquaye <racquaye89@gmail.com>
2026-03-26 23:20:09 +0000

    style (project): add transparent rounded fills around tech list items

--- components/projects/sections/TechList.tsx
+++ components/projects/sections/TechList.tsx
@@ -6,7 +6,7 @@ export default function TechList({ techStack }: { techStack: string[] }) {
       <SectionHeading showIcon={false}>Technologies</SectionHeading>
       <ul className="flex gap-x-4 gap-y-2 flex-wrap text-[rgb(var(--txt-list-item))]">
         {techStack.map((tech: string, index: number) => (
-          <li className="label tech-list__tech" key={index}>
+          <li className="label tech-list__tech bg-[rgba(var(--txt-body))]/[.15] px-2 rounded-xl" key={index}>
             {tech}
           </li>
         ))}
