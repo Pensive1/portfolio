0770b3d28616d89b5518dc0f99aad7fa8e550b84
Richard Acquaye <racquaye89@gmail.com>
2026-04-01 16:55:38 +0100

    refactor: increase section header icon size + padding

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -5,11 +5,11 @@ import Icon from "./Icon";
 export default function SectionHeading({
   showIcon = true,
   iconType,
-  size = 2,
+  size = 3,
   children,
 }: SectionHeadingProps) {
   return (
-    <div className="flex items-center gap-3 mb-3 md:mb-4">
+    <div className="flex items-center gap-4 mb-3 md:mb-4">
       {showIcon && <Icon iconType={iconType} size={size} use="sectionHeading" />}
       <h3 className="text-[rgb(var(--txt-section-title))]">{children}</h3>
     </div>
